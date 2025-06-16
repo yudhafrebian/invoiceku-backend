569e721f4bb1996b9416e59dfa4fc5793a7f8b1d
Yudha Frebian <anandayudha777@gmail.com>
2025-06-16 23:48:09 +0700

    testing cron job

--- src/cronJob.ts
+++ src/cronJob.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const BACKEND_URL = process.env.BASE_URL || "http://localhost:4000";
 
-cron.schedule("*/2 * * * *", async () => {
+cron.schedule("0 23 * * *", async () => {
   try {
     console.log("Cron running: sending scheduled email invoice...");
     const response = await axios.post(`${BACKEND_URL}/invoice/send-email-auto`);
