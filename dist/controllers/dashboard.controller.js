e515c0da7dffe6843f8ed2ee7e8a40bfcb712530
yudhafrebian <anandayudha777@gmail.com>
2025-06-25 13:35:16 +0700

    fix: bug on class dashboard

--- dist/controllers/dashboard.controller.js
+++ dist/controllers/dashboard.controller.js
@@ -1,16 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getSummary = void 0;
 const response_1 = require("../utils/response");
 const dashboard_service_1 = require("../services/dashboard.service");
-const getSummary = async (req, res, next) => {
-    try {
-        const userId = res.locals.data.id;
-        const summary = await (0, dashboard_service_1.getSummaryService)(userId);
-        (0, response_1.successResponse)(res, "Success", summary);
+class DashboardController {
+    async getSummary(req, res, next) {
+        try {
+            const userId = res.locals.data.id;
+            const summary = await (0, dashboard_service_1.getSummaryService)(userId);
+            (0, response_1.successResponse)(res, "Success", summary);
+        }
+        catch (error) {
+            next(error);
+        }
     }
-    catch (error) {
-        next(error);
-    }
-};
-exports.getSummary = getSummary;
+}
+exports.default = DashboardController;
--- src/controllers/dashboard.controller.ts
+++ src/controllers/dashboard.controller.ts
@@ -1,19 +1,25 @@
-import { Request, Response, NextFunction } from "express";
-import { successResponse } from "../utils/response";
+import { Response, Request, NextFunction } from "express";
+import prisma from "../configs/prisma";
+import { createResponse, successResponse } from "../utils/response";
 import { getSummaryService } from "../services/dashboard.service";
 
-export const getSummary = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-): Promise<void> => {
-  try {
-    const userId = res.locals.data.id;
-
-    const summary = await getSummaryService(userId);
-
-    successResponse(res, "Success", summary);
-  } catch (error) {
-    next(error);
+class DashboardController {
+  async getSummary(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const userId = res.locals.data.id;
+  
+      const summary = await getSummaryService(userId);
+  
+      successResponse(res, "Success", summary);
+    } catch (error) {
+      next(error);
+    }
   }
-};
+}
+
+
+export default DashboardController;
