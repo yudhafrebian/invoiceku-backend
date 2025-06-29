33b354da173c34dfd09faf834f77d4cfce17022c
yudhafrebian <anandayudha777@gmail.com>
2025-06-30 01:00:34 +0700

    debugging cron job

--- src/cronJob.ts
+++ src/cronJob.ts
@@ -2,7 +2,7 @@ import cron from "node-cron";
 import { scheduledEmailLogic, markOverdueInvoices } from "./utils/scheduledEmailLogic";
 import { handleRecurringInvoice } from "./utils/handleRecurringInvoice";
 
-cron.schedule("0 0 * * *", async () => {
+cron.schedule("02 1 * * *", async () => {
   try {
     console.log("Cron running: sending scheduled emails, marking overdue, handling recurring...");
     const countOverdue = await markOverdueInvoices();
