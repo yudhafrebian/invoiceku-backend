bf0c954da63463d3a257687b5af7a932924fbda9
Yudha Frebian <anandayudha777@gmail.com>
2026-02-10 13:34:51 +0700

    add origin site

--- src/app.ts
+++ src/app.ts
@@ -27,7 +27,7 @@ class App {
   private configure(): void {
     this.app.use(
       cors({
-        origin: "https://anandayudha.my.id",
+        origin: ["https://anandayudha.my.id","https://invoiceku-purwadhika.vercel.app"],
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
       })
     );
